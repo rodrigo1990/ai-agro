4056624af4117ae0c70a01ab013ed08dc64791a0
RodrigoReynoso <mcd77.1990@gmail.com>
2026-04-06 02:46:02 +0200

    Integrate Laravel Sanctum for API authentication and token management.

--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,10 @@ use App\Http\Controllers\SocietyController;
 use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
 Route::get('/societies',     [SocietyController::class,     'getAllSocieties']);
 Route::get('/farmers',       [FarmerController::class,       'getAllFarmers']);
 Route::get('/establishments',[EstablishmentController::class,'getAllEstablishments']);

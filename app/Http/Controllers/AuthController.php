3136188cba717c93bf029d874d1a46c2ad9e1f90
RodrigoReynoso <mcd77.1990@gmail.com>
2026-04-06 15:22:13 +0200

    Handle validation exceptions in AuthController with custom JSON response for invalid email.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,15 +6,21 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
     public function login(Request $request): JsonResponse
     {
-        $credentials = $request->validate([
-            'email' => ['required', 'email'],
-            'password' => ['required'],
-        ]);
+        try {
+            $credentials = $request->validate([
+                'email' => ['required', 'email'],
+                'password' => ['required'],
+            ]);
+        }catch (ValidationException $e) {
+            if($e->status === 422)
+                return response()->json(['success' => false, 'message' => 'Email is invalid'], 401);
+        }
 
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();

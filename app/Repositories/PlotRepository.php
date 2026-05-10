157a213a2338d2c8c725a000cbfa14cc06d55ab2
RodrigoReynoso <mcd77.1990@gmail.com>
2026-05-10 14:48:53 +0200

    Add endpoints for fetching a plot or establishment by ID and creating/updating them with saveOrUpdate methods

--- app/Repositories/PlotRepository.php
+++ app/Repositories/PlotRepository.php
@@ -12,4 +12,35 @@ class PlotRepository implements PlotRepositoryInterface
     {
         return Plot::all();
     }
+
+    public function findById(int $id): ?Plot
+    {
+        return Plot::find($id);
+    }
+
+    public function saveOrUpdate(array $data): ?Plot
+    {
+        if (isset($data['id'])) {
+            $plot = Plot::find($data['id']);
+            if ($plot === null) {
+                return null;
+            }
+        } else {
+            $plot = new Plot();
+        }
+
+        $plot->farmer_id        = $data['farmer_id'];
+        $plot->establishment_id = $data['establishment_id'];
+        $plot->name             = $data['name'];
+        $plot->active           = $data['active'] ?? true;
+        $plot->area             = $data['area'];
+        $plot->latitude         = $data['latitude'];
+        $plot->longitude        = $data['longitude'];
+        $plot->external_code    = $data['external_code'] ?? null;
+        $plot->polygon          = $data['polygon'] ?? null;
+
+        $plot->save();
+
+        return $plot;
+    }
 }
--- app/Services/PlotService.php
+++ app/Services/PlotService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Models\Plot;
 use App\Repositories\Contracts\PlotRepositoryInterface;
 use Illuminate\Database\Eloquent\Collection;
 
@@ -14,5 +15,15 @@ class PlotService
     public function getAllPlots(): Collection
     {
         return $this->repository->getAll();
+    }
+
+    public function getPlotById(int $id): ?Plot
+    {
+        return $this->repository->findById($id);
+    }
+
+    public function saveOrUpdate(array $data): ?Plot
+    {
+        return $this->repository->saveOrUpdate($data);
     }
 }
